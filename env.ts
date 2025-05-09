643106863678f3093b3703ba303069486692cbb8
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-05-08 23:27:03 -0300

    add different map library

--- env.ts
+++ env.ts
@@ -3,7 +3,8 @@ import { z } from "zod";
 const envSchema = z.object({
   VITE_API_URL: z.string().url(),
   VITE_GOOGLE_API_URL: z.string(),
-  VITE_API_TOKEN: z.string()
+  VITE_API_TOKEN: z.string(),
+  VITE_MAP_ID: z.string()
 });
 
 export const env = envSchema.parse(import.meta.env);
--- src/pages/Home/CarsMap.tsx
+++ src/pages/Home/CarsMap.tsx
@@ -1,77 +1,72 @@
 import { useEffect, useState } from "react";
-import { GoogleMap, LoadScript, Marker, InfoWindow } from "@react-google-maps/api";
-import { env } from './../../../env';
+import {
+  APIProvider,
+  Map,
+  InfoWindow,
+  Marker,
+} from "@vis.gl/react-google-maps";
+import { env } from "./../../../env";
 import { LocationVehicle } from "@/api/get-vehicles";
-import greenVehicleMarkerIcon from './../../assets/green-vehicle-marker-icon.svg'
-import redVehicleMarkerIcon from './../../assets/red-vehicle-marker-icon.svg'
+import greenVehicleMarkerIcon from "./../../assets/green-vehicle-marker-icon.svg";
+import redVehicleMarkerIcon from "./../../assets/red-vehicle-marker-icon.svg";
 import { VehicleInfo } from "./VehicleInfo";
-
-const containerStyle = {
-  width: '100%',
-  height: '300px',
-  borderRadius:"10px"
-};
-
-const customMapStyle = [];
 
 
 type Props = {
-  trackedVehicles:LocationVehicle[]
-}
-export const CarsMap = ({trackedVehicles}:Props) => {
-  
+  trackedVehicles: LocationVehicle[];
+};
+export const CarsMap = ({ trackedVehicles }: Props) => {
   const [selectedVehicle, setSelectedVehicle] = useState<LocationVehicle | null>(null);
-  const [center, setCenter] = useState<{ lat: number; lng: number }>({lat:0, lng:0})
-  
-  useEffect(()=>{
-    if(trackedVehicles.length > 0){
-      const {lat, lng} = trackedVehicles[0]
-      setCenter({lat, lng})
+  const [center, setCenter] = useState<{ lat: number; lng: number } | null>(null);
+
+  useEffect(() => {
+    if (trackedVehicles.length > 0) {
+      const { lat, lng } = trackedVehicles[0];
+      setCenter({ lat, lng });
     }
-  },[trackedVehicles])
-  function getMarkerBasedOnVehicleStatus(status:string){
+  }, [trackedVehicles]);
+  function getMarkerBasedOnVehicleStatus(status: string) {
     const ignitionStatuses = {
-      "Ligado": greenVehicleMarkerIcon,
-      "Desligado": redVehicleMarkerIcon
-    }
-    return ignitionStatuses[status]
-    
+      Ligado: greenVehicleMarkerIcon,
+      Desligado: redVehicleMarkerIcon,
+    };
+    return ignitionStatuses[status];
   }
   return (
-    <div className="flex flex-col p-4 bg-accent rounded-lg gap-2">
-    <h1 className="font-poppins text-white font-semibold">Mapa rastreador</h1>
-    <LoadScript googleMapsApiKey={env.VITE_GOOGLE_API_URL}>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={5}
-        options={{ styles: customMapStyle }}
-      >
-        {trackedVehicles.map((vehicle, index) => {
-          const {lat,lng, ignition} = vehicle
-          return(
-          <Marker
-            key={index}
-            position={{lat, lng}}
-            icon={{
-              url: getMarkerBasedOnVehicleStatus(ignition) ,
-            }}
-            onClick={() => setSelectedVehicle(vehicle)}
-          />
-        )})}
-
-        {selectedVehicle && (
-          <InfoWindow
-            position={selectedVehicle}
-            onCloseClick={() => setSelectedVehicle(null)}
-            children={<VehicleInfo vehicle={selectedVehicle} />}
-            
+    <APIProvider apiKey={env.VITE_GOOGLE_API_URL}>
+      <div className="flex flex-col p-4 bg-accent rounded-lg gap-2">
+        <h1 className="font-poppins text-white font-semibold">Mapa rastreador</h1>
+        {center && (
+          <Map
+            defaultCenter={center}
+            defaultZoom={4}
+            style={{ height: "300px", width: "100%", borderRadius: "10px" }}
+            disableDefaultUI
+            mapId={env.VITE_MAP_ID}
           >
-          </InfoWindow>
+            {trackedVehicles.map((vehicle, index) => {
+              const { lat, lng, ignition } = vehicle;
+
+              return (
+                <div key={index}>
+                  <Marker
+                    position={{ lat, lng }}
+                    onClick={() => setSelectedVehicle(vehicle)}
+                    icon={{
+                      url: getMarkerBasedOnVehicleStatus(ignition),
+                    }}
+                  />
+                </div>
+              );
+            })}
+            {selectedVehicle && (
+              <InfoWindow onClose={() => setSelectedVehicle(null)} position={selectedVehicle}>
+                <VehicleInfo vehicle={selectedVehicle} />
+              </InfoWindow>
+            )}
+          </Map>
         )}
-      </GoogleMap>
-    </LoadScript>
-    </div>
+      </div>
+    </APIProvider>
   );
 };
-
