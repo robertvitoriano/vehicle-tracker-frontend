73f8819b0d5737f0cb1601038f7a15a1984315bb
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-05-09 20:50:10 -0300

    remove mock row

--- src/pages/Home/VehicleTable.tsx
+++ src/pages/Home/VehicleTable.tsx
@@ -25,13 +25,6 @@ export const VehicleTable = ({ vehicles }: Props) => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableCell>EAD 7328</TableCell>
-            <TableCell>000001</TableCell>
-            <TableCell>Motor</TableCell>
-            <TableCell>FH 460</TableCell>
-            <TableCell>Em viagem</TableCell>
-          </TableRow>
           {vehicles.map(({ plate, fleet, type, model, status }) => (
             <TableRow>
               <TableCell>{plate}</TableCell>
