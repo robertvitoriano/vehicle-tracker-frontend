b704eee7f02c5c32c38b8ff41fb505f33e30ac2f
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-05-10 10:03:01 -0300

    remove deprecated function from test

--- src/hooks/useInfiniteScroll.spec.ts
+++ src/hooks/useInfiniteScroll.spec.ts
@@ -1,5 +1,5 @@
 import { renderHook } from "@testing-library/react";
-import { act } from "react-dom/test-utils";
+import { act } from "react";
 import { useInfiniteScroll } from "./useInfiniteScroll";
 
 describe("useInfiniteScroll Hook", () => {
