2ee462e6669497afe0d1b6c8804d97704973ba69
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-05-10 09:22:10 -0300

    refactor home e2e tests

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,19 +1,30 @@
+const API_URL = "https://develop-back-rota.rota361.com.br/recruitment/vehicles/list-with-paginate";
+const PAGE_CONTAINER_SELECTOR = ".page-container";
+
 describe("Home Page", () => {
-  it("should call the vehicles API on page load", () => {
-    cy.intercept("GET", "https://develop-back-rota.rota361.com.br/recruitment/vehicles/list-with-paginate*").as("getVehicles");
-
+  beforeEach(() => {
+    
+    cy.intercept("GET", `${API_URL}?type=tracked&page=1&perPage=20`).as("getVehicles");
     cy.visit("/");
+  });
 
-    cy.wait("@getVehicles").its("response.statusCode").should("eq", 200);
+  context("Initial Page Load", () => {
+    it("should call the vehicles API on page load", () => {
+      
+      cy.wait("@getVehicles").its("response.statusCode").should("eq", 200);
+      
+    });
   });
-  
-  it("should call the vehicles API on page load and after scrolling down", () => {
-    cy.intercept("GET", "https://develop-back-rota.rota361.com.br/recruitment/vehicles/list-with-paginate?type=tracked&page=1&perPage=20").as("getVehicles");
-    cy.intercept("GET","https://develop-back-rota.rota361.com.br/recruitment/vehicles/list-with-paginate?type=tracked&page=2&perPage=20").as("getVehiclesAfterScroll")
-    cy.visit("/");
-    cy.wait("@getVehicles").its("response.statusCode").should("eq", 200);
-    cy.get(".page-container").scrollTo("bottom");
-    cy.wait("@getVehiclesAfterScroll").its("response.statusCode").should("eq", 200);
+
+  context("Infinite Scrolling", () => {
+    it("should call the vehicles API after scrolling down", () => {
+      cy.intercept("GET", `${API_URL}?type=tracked&page=2&perPage=20`).as("getVehiclesAfterScroll");
+
+      cy.wait("@getVehicles").its("response.statusCode").should("eq", 200);
+
+      cy.get(PAGE_CONTAINER_SELECTOR).scrollTo("bottom");
+
+      cy.wait("@getVehiclesAfterScroll").its("response.statusCode").should("eq", 200);
+    });
   });
-  
 });
