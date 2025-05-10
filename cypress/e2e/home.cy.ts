8d044fa672cfaf95a184d9aae9e2149f7d131703
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-05-10 09:14:50 -0300

    test page infinite scroll

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -7,4 +7,13 @@ describe("Home Page", () => {
     cy.wait("@getVehicles").its("response.statusCode").should("eq", 200);
   });
   
+  it("should call the vehicles API on page load and after scrolling down", () => {
+    cy.intercept("GET", "https://develop-back-rota.rota361.com.br/recruitment/vehicles/list-with-paginate?type=tracked&page=1&perPage=20").as("getVehicles");
+    cy.intercept("GET","https://develop-back-rota.rota361.com.br/recruitment/vehicles/list-with-paginate?type=tracked&page=2&perPage=20").as("getVehiclesAfterScroll")
+    cy.visit("/");
+    cy.wait("@getVehicles").its("response.statusCode").should("eq", 200);
+    cy.get(".page-container").scrollTo("bottom");
+    cy.wait("@getVehiclesAfterScroll").its("response.statusCode").should("eq", 200);
+  });
+  
 });
